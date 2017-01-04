4b24dd188f7fd30dd619b017cc10c864a6af1776
Joshua Simmons <joshua.simmons@emptypath.com>
2017-01-03 22:45:56 -0700

    create initial basic files

---
+++ test/Test.hs
@@ -0,0 +1,15 @@
+-- |
+-- Module      :  Main
+-- Copyright   :  Joshua Simmons 2017
+-- License     :  BSD3
+--
+-- Maintainer  :  joshua.simmons@emptypath.com
+--
+-- suffix-array test-suite using tasty and etc.
+--
+module Main
+( main
+) where
+
+main :: IO ()
+main = return ()
