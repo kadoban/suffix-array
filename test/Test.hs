b6560eb71b89b5bac23cdcb99c860dc67faea6fa
Joshua Simmons <joshua.simmons@emptypath.com>
2017-01-03 23:45:36 -0700

    test the 'suffixes' function

--- test/Test.hs
+++ test/Test.hs
@@ -11,5 +11,23 @@ module Main
 ( main
 ) where
 
+import           Test.Tasty
+import qualified Test.Tasty.QuickCheck as QC
+
+import           Data.List (tails)
+
+import           Data.SuffixArray.Internal
+
 main :: IO ()
-main = return ()
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests" [basics]
+
+basics :: TestTree
+basics = testGroup "tests of basic underlying utils and etc."
+  [ QC.testProperty "suffixes == filter (not . null) tails" $
+      \xs -> suffixes (xs :: [Int]) == filter (not . null) (tails xs)
+  , QC.testProperty "(length . suffixes) == length" $
+      \xs -> length (suffixes xs) == length (xs :: [Int])
+  ]
