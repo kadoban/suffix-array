8ee7d6bc5734258f86abfcedd430b1ab52c2d9fd
Joshua Simmons <joshua.simmons@emptypath.com>
2017-01-04 15:26:09 -0700

    add Alpha and prepare util stuff

--- src/Data/SuffixArray/Internal.hs
+++ src/Data/SuffixArray/Internal.hs
@@ -11,7 +11,10 @@
 -- to be relied upon for any reason.
 --
 module Data.SuffixArray.Internal
-( suffixes
+( Alpha(..)
+, prepare
+, prepareOne
+, suffixes
 ) where
 
 -- | Yields the non-empty suffixes of a list in order of decreasing length.
@@ -21,3 +24,27 @@ module Data.SuffixArray.Internal
 suffixes :: [a] -> [[a]]
 suffixes xxs@(x:xs) = xxs : suffixes xs
 suffixes [] = []
+
+-- | A character in a string (or set of strings) we're going to compute the
+-- suffix array of.
+-- Includes `Sentinal` markers for the end of strings.
+data Alpha a = Sentinal Int -- ^ Used to mark the end of a string.
+                            -- The `Int` parameter is used to encode
+                            -- which string this is the end of, in cases
+                            -- where there are multiple.
+             | Alpha a -- ^ An actual character in the string.
+    deriving (Eq, Ord, Show)
+
+-- | Convenience value containing `Sentinal`s in order.
+sentinals :: [Alpha a]
+sentinals = map Sentinal [0..]
+
+-- | Prepare a list of strings to compute the suffix array of them.
+-- Just wraps every character in `Alpha` and adds `Sentinal`s to the end of
+-- each string, and concatenates it together.
+prepare :: [[a]] -> [Alpha a]
+prepare = concat . zipWith (\a b -> b ++ [a]) sentinals . map (map Alpha)
+
+-- | Convenience function to `prepare` a single string.
+prepareOne :: [a] -> [Alpha a]
+prepareOne = prepare . pure
