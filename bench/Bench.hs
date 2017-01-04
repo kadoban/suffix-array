ed4ae25520059af5252c67db6b032df0d4f23b50
Joshua Simmons <joshua.simmons@emptypath.com>
2017-01-04 16:20:45 -0700

    add benchmarks

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -11,5 +11,35 @@ module Main
 ( main
 ) where
 
+import           Criterion.Main
+import           System.Random (newStdGen, randoms)
+
+import           Data.SuffixArray.Internal
+
 main :: IO ()
-main = return ()
+main = do
+  g <- newStdGen
+  let rands, sorts, revs :: [Int]
+      rands = randoms g
+      sorts = [1..]
+      revs = [1000000, 999999 ..]
+      reps = concatMap (\x -> replicate x x) [1..]
+  defaultMain
+   [
+    bgroup "single_suffixes"
+    [ bench (unwords [show (sz, k), dist', var'])
+          $ whnf (\(n,k) -> var (take n (map (`mod` k) dist))) (sz, k)
+    | (dist, dist') <- [ (rands, "rands"), (sorts, "sorts")
+                       , (revs, "revs"), (reps, "reps")]
+    , k <- [5, 40, 90, 200, 1000]
+    , sz <- [5000, 10000 .. 100000] ++ [200000]
+    , (var, var') <- [(naiveOne, "naiveOne")]
+    , interesting var' sz k dist'
+    ]
+   ]
+
+interesting "naiveOne" n k v
+  | n > 40000 && v `elem` ["sorts", "revs"] = False
+  | n > 100000 = False
+  | otherwise = True
+interesting _ _ _ _ = True
