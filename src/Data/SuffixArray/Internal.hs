279c180bdee4a6f83d461f66db7b05e9fc1dd895
Joshua Simmons <joshua.simmons@emptypath.com>
2017-01-03 23:23:15 -0700

    add 'suffixes' function for a list

--- src/Data/SuffixArray/Internal.hs
+++ src/Data/SuffixArray/Internal.hs
@@ -11,5 +11,13 @@
 -- to be relied upon for any reason.
 --
 module Data.SuffixArray.Internal
-(
+( suffixes
 ) where
+
+-- | Yields the non-empty suffixes of a list in order of decreasing length.
+--
+-- This differs from `Data.List.tails` in that it does not include the
+-- empty list at the end.
+suffixes :: [a] -> [[a]]
+suffixes xxs@(x:xs) = xxs : suffixes xs
+suffixes [] = []
