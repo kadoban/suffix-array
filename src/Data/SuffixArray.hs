3d849017f414d8e6b9a5e0dbc1562209232cb133
Joshua Simmons <joshua.simmons@emptypath.com>
2017-01-03 23:14:40 -0700

    create basic library modules

---
+++ src/Data/SuffixArray.hs
@@ -0,0 +1,12 @@
+-- |
+-- Module      :  Data.SuffixArray
+-- Copyright   :  Joshua Simmons 2017
+-- License     :  BSD3
+--
+-- Maintainer  :  joshua.simmons@emptypath.com
+--
+-- Suffix array library main module
+--
+module Data.SuffixArray
+(
+) where
---
+++ src/Data/SuffixArray/Internal.hs
@@ -0,0 +1,15 @@
+-- |
+-- Module      :  Data.SuffixArray.Internal
+-- Copyright   :  Joshua Simmons 2017
+-- License     :  BSD3
+--
+-- Maintainer  :  joshua.simmons@emptypath.com
+--
+-- Stability   :  unstable
+--
+-- Internal implementation details, unstable and not
+-- to be relied upon for any reason.
+--
+module Data.SuffixArray.Internal
+(
+) where
